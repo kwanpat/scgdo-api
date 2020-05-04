0b1ff2c3ea367b5bc5cee9f43a301e95b4647b6b
Joe <33779936+kwanpat@users.noreply.github.com>
2020-05-05 01:01:01 +0700

    remove api key

--- src/config/env.js
+++ src/config/env.js
@@ -6,7 +6,7 @@ const options = {
   PORT: num({ default: 8000 }),
   LOG_LEVEL: str({ default: "info" }),
   DEFAULT_API_VERSION: num({ default: 1 }),
-  GOOGLE_MAPS_API_KEY: str({ default: "AIzaSyAre3NCleRE9xeNYgCXGjqV6V-RcLqiWVQ" })
+  GOOGLE_MAPS_API_KEY: str({ default: "" })
 };
 
 module.exports = envalid.cleanEnv(process.env, options);
